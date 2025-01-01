cc5c675070faef5c2e409c87ea817fba4e9dcd18
Lamis <iberlamis79@gmail.com>
2025-01-01 23:40:17 +0100

    update transactions

--- RealEstate/src/model/Transaction.java
+++ RealEstate/src/model/Transaction.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
@@ -20,7 +21,6 @@ public class Transaction {
     private Date dateFin;
     private double montantPaiement;
 
-
     public Transaction(TransactionType transactionType, String idTransaction, String initiatorId, String recipientId, double price, Status status, Date date, Date dateDebut, Date dateFin, double montantPaiement) {
         this.transactionType = transactionType;
         this.idTransaction = idTransaction;
@@ -34,7 +34,6 @@ public class Transaction {
         this.montantPaiement = montantPaiement;
     }
 
-    
     public TransactionType getTransactionType() {
         return transactionType;
     }
@@ -114,4 +113,46 @@ public class Transaction {
     public void setMontantPaiement(double montantPaiement) {
         this.montantPaiement = montantPaiement;
     }
-}
+
+    public String toFileFormat() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return transactionType + "," + idTransaction + "," + initiatorId + "," + recipientId + "," + price + "," + status + "," + sdf.format(date) + "," + sdf.format(dateDebut) + "," + sdf.format(dateFin) + "," + montantPaiement;
+    }
+
+    public static Transaction fromFileFormat(String line) {
+        String[] parts = line.split(",");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            TransactionType transactionType = TransactionType.valueOf(parts[0]);
+            String idTransaction = parts[1];
+            String initiatorId = parts[2];
+            String recipientId = parts[3];
+            double price = Double.parseDouble(parts[4]);
+            Status status = Status.valueOf(parts[5]);
+            Date date = sdf.parse(parts[6]);
+            Date dateDebut = sdf.parse(parts[7]);
+            Date dateFin = sdf.parse(parts[8]);
+            double montantPaiement = Double.parseDouble(parts[9]);
+            return new Transaction(transactionType, idTransaction, initiatorId, recipientId, price, status, date, dateDebut, dateFin, montantPaiement);
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la conversion de la ligne en transaction : " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return "Transaction{" +
+                "transactionType=" + transactionType +
+                ", idTransaction='" + idTransaction + '\'' +
+                ", initiatorId='" + initiatorId + '\'' +
+                ", recipientId='" + recipientId + '\'' +
+                ", price=" + price +
+                ", status=" + status +
+                ", date=" + sdf.format(date) +
+                ", dateDebut=" + sdf.format(dateDebut) +
+                ", dateFin=" + sdf.format(dateFin) +
+                ", montantPaiement=" + montantPaiement +
+                '}';
+    }}
